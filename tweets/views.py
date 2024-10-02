c3add6523446f365092b55d8b097dd79cfa90e62
Jinho Jeong <zinojung@users.noreply.github.com>
2024-10-02 17:01:47 +0900

    render all tweets using render

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Tweet
 
-# Create your views here.
+
+def see_all_tweets(request):
+    tweets = Tweet.objects.all()
+    return render(
+        request,
+        "all_tweets.html",
+        {
+            "tweets": tweets,
+            "title": "Hello! this title comes from django!",
+        }
+    )
